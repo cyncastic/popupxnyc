e2dce5f0b36891f2d1adf235011faee4b7216b0f
sedicted <john@cyncastic.local>
2013-12-14 17:11:09 -0500

    add artist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214040943) do
+ActiveRecord::Schema.define(version: 20131214220826) do
 
   create_table "artworks", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131214040943) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "price"
+    t.string   "artist"
   end
 
   create_table "blogs", force: true do |t|
